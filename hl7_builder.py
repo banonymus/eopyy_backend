08d401ffb34d205490c5b6ebce0b2bbad63562f9
babis <babis@anonymus.gr>
2026-08-16 14:06:44 +0300

    updated backend NEW 15/8/26 Admissions PV1 fix and return result to postman without saving to database

--- hl7_builder.py
+++ hl7_builder.py
@@ -125,8 +125,8 @@ def build_PV1(location_code, visit_number, admit_datetime, discharge_datetime,
 
     # PV1.5–PV1.18 empty → 15 pipes after location_code
 
-    pv1[21] = patient_type     # PV1.19
-    pv1[22] = visit_number     # PV1.20
+    pv1[12] = patient_type     # PV1.19
+    pv1[13] = visit_number     # PV1.20
 
     pv1[42] = discharge_datetime  # PV1.42
 

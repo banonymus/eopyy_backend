6382c9d8cff5d2d6a8c7c1a3a7dd40c5ac6104fb
babis <babis@anonymus.gr>
2026-08-17 16:04:16 +0300

    updated backend NEW 15/8/26 Admissions PV1 fix and return result to postman without saving to database

--- hl7_builder.py
+++ hl7_builder.py
@@ -130,7 +130,8 @@ def build_PV1(location_code, doctor_code, ticket_number, admit_datetime, alt_vis
     pv1[44] = admit_datetime    # PV1.44 Admit Date/Time
 
     # PV1.50 (index 49)
-    pv1[49] = alt_visit_id if alt_visit_id else ticket_number
+    #pv1[49] = alt_visit_id if alt_visit_id else ticket_number
+    pv1[49] = ticket_number+1
 
     return "|".join(pv1)
 
